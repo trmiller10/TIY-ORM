cde20b75e8e7b3a30b4b720a2fe65d1ee5da1997
Taylor Miller <trmiller10@gmail.com>
2016-05-27 10:02:32 -0400

    Edited application.properties. connected PostgreSQL database, created classes and interfaces

---
+++ src/main/java/com/theironyard/Customer.java
@@ -0,0 +1,7 @@
+package com.theironyard;
+
+/**
+ * Created by Taylor on 5/27/16.
+ */
+public class Customer {
+}
---
+++ src/main/java/com/theironyard/PurchaseRepository.java
@@ -0,0 +1,9 @@
+package com.theironyard;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Taylor on 5/27/16.
+ */
+public interface PurchaseRepository extends CrudRepository{
+}
